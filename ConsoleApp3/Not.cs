ca235e9eaf74413465ae3b38ee40906dd4352ebb
ege aydın <egeaydinn31@gmail.com>
2024-05-28 16:48:52 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp3/Not.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siniflar_odev_1
+{
+    internal class Not
+    {
+        public int Vize { get; set; }
+        public int Final { get; set; }
+
+        public void VizeFinalBelirle(int v, int f)
+        {
+            // Ödev
+            /* 
+             Not adında bir sınıf oluşturulacak. Propertyleri Vize ve Final olacak.
+             Methodları VizeFinalBelirle(int v, int f) , OrtalamaAl adlı geriye double döndürecek ve Öğrencinin Vize,Final ve Ortalaması ekrana yazdıracak methodlar olacak.
+             */
+
+            Final = v;
+            Vize = f;
+
+        }
+        public double OrtalamaAl()
+        {
+
+            double sonuc = (Final + Vize) / 2;
+
+            return sonuc;
+        }
+
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siniflar_odev_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ödev
+            /* 
+             Not adında bir sınıf oluşturulacak. Propertyleri Vize ve Final olacak.
+             Methodları VizeFinalBelirle(int v, int f) , OrtalamaAl adlı geriye double döndürecek ve Öğrencinin Vize,Final ve Ortalaması ekrana yazdıracak methodlar olacak.
+                 */
+
+            Console.WriteLine("Vize Notunuzu Giririnz");
+            int vize = int.Parse(Console.ReadLine());
+            Console.WriteLine("Final Notunuzu Giririnz");
+            int final = int.Parse(Console.ReadLine());
+
+            Not not = new Not();
+            not.Vize = vize;
+            not.Final = final;
+            not.VizeFinalBelirle(vize, final);
+            Console.WriteLine("vize ve final ortalamanız: {0}", not.OrtalamaAl());
+
+            Console.ReadLine();
+
+        }
+    }
+}
